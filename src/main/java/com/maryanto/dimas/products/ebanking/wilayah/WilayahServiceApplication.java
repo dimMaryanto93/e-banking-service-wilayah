2bb077766e5c01943f17996ca81c02bc0425fb21
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-08-12 20:40:22 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/products/ebanking/wilayah/WilayahServiceApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.products.ebanking.wilayah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilayahServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WilayahServiceApplication.class, args);
+    }
+}
